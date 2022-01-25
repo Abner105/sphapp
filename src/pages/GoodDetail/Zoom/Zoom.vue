1faa0a7599056711ce596621e1770853028e359e
Abner105 <1261087446@qq.com>
2022-01-25 22:23:49 +0800

    完成商品详情页的放大镜功能、轮播图与切换属性

--- src/pages/GoodDetail/Zoom/Zoom.vue
+++ src/pages/GoodDetail/Zoom/Zoom.vue
@@ -1,84 +1,113 @@
 <template>
   <div class="spec-preview" v-if="skuImageList && skuImageList[0]">
-    <img :src="skuImageList[0].imgUrl" />
-    <div class="event"></div>
+    <img :src="skuImageList[currentIndex].imgUrl" />
+    <div class="event" @mousemove="magnifier"></div>
     <div class="big">
-      <img :src="skuImageList[0].imgUrl" />
+      <img :src="skuImageList[currentIndex].imgUrl" ref="big" />
     </div>
-    <div class="mask"></div>
+    <div class="mask" ref="mask"></div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Zoom",
-    props:{
-      skuImageList:{
-        type:Array,
-        default(){
-          return []
-        }
-      }
-    }
-  }
+export default {
+  name: "Zoom",
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  props: {
+    skuImageList: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  mounted() {
+    this.$bus.$on("getCurrentIndex", (index) => {
+      this.currentIndex = index;
+    });
+  },
+  methods: {
+    // 放大镜
+    magnifier(event) {
+      // console.log(event)
+      let left = event.offsetX - this.$refs.mask.offsetWidth / 2;
+      let top = event.offsetY - this.$refs.mask.offsetHeight / 2;
+      if (left < 0) left = 0;
+      if (top < 0) top = 0;
+      if (left > this.$refs.mask.offsetWidth)
+        left = this.$refs.mask.offsetWidth;
+      if (top > this.$refs.mask.offsetHeight)
+        top = this.$refs.mask.offsetHeight;
+      this.$refs.mask.style.left = left + "px";
+      this.$refs.mask.style.top = top + "px";
+      this.$refs.big.style.top = - 2 * top + "px";
+      this.$refs.big.style.left = - 2* left + "px";
+
+    },
+  },
+};
 </script>
 
 <style lang="less">
-  .spec-preview {
-    position: relative;
-    width: 400px;
-    height: 400px;
-    border: 1px solid #ccc;
+.spec-preview {
+  position: relative;
+  width: 400px;
+  height: 400px;
+  border: 1px solid #ccc;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .event {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 998;
+  }
+
+  .mask {
+    width: 50%;
+    height: 50%;
+    background-color: rgba(0, 255, 0, 0.3);
+    position: absolute;
+    left: 0;
+    top: 0;
+    display: none;
+  }
+
+  .big {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: -1px;
+    left: 100%;
+    border: 1px solid #aaa;
+    overflow: hidden;
+    z-index: 998;
+    display: none;
+    background: white;
 
     img {
-      width: 100%;
-      height: 100%;
-    }
-
-    .event {
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      top: 0;
-      left: 0;
-      z-index: 998;
-    }
-
-    .mask {
-      width: 50%;
-      height: 50%;
-      background-color: rgba(0, 255, 0, 0.3);
+      width: 200%;
+      max-width: 200%;
+      height: 200%;
       position: absolute;
       left: 0;
       top: 0;
-      display: none;
-    }
-
-    .big {
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      top: -1px;
-      left: 100%;
-      border: 1px solid #aaa;
-      overflow: hidden;
-      z-index: 998;
-      display: none;
-      background: white;
-
-      img {
-        width: 200%;
-        max-width: 200%;
-        height: 200%;
-        position: absolute;
-        left: 0;
-        top: 0;
-      }
-    }
-
-    .event:hover~.mask,
-    .event:hover~.big {
-      display: block;
     }
   }
+
+  .event:hover ~ .mask,
+  .event:hover ~ .big {
+    display: block;
+  }
+}
 </style>
